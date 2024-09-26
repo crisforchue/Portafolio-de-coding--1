a92d50577031190a51e2d1df88da2441d3367197
CrisForchue <forchuel@gmail.com>
2024-09-26 09:42:38 -0400

    leccion (deayer) 25/sept

---
+++ BotDiscord.py
@@ -0,0 +1,22 @@
+from discord_easy_commands import EasyBot
+import pyjokes
+import discord
+import random
+
+token = 'MTI4ODY3MjUwNzAzMjc2ODU1Ng.GELGCS.-xkUv8feP5sEK97tw2qm_9As-Z5cnSI-u58RGc'
+
+jokes = pyjokes.get_jokes(language='en', category='all')
+intents = discord.Intents.all()
+bot = EasyBot(intents=intents)
+
+def random_joke():
+    joke = random.choice(jokes)
+    return joke
+
+bot.setCommand("!youtube", 'https://www.youtube.com')
+bot.setCommand("!x", 'https://x.com/home')
+bot.setCommand("!jokes", random_joke())
+
+bot.run(token)
+
+print("corriendo...")
